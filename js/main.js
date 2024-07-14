2ab306a1292e2850d9db28a9da8b1a357a90a8f2
troisterre <a.kamimura.troisterre@gmail.com>
2024-07-15 00:21:05 +0900

    7/15アニメーション修正

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $(".hamburger").on("click", function () {
   $(".hamburger").toggleClass("active");
   $(".p-hamburger__menu").toggleClass("open");
-  $(".p-hamburger__menu").slideToggle(500);
+  $(".p-hamburger__menu").slideToggle();
 });
-
+$(window).resize(function () {
+  $(".hamburger").removeClass("active");
+  $(".p-hamburger__menu").removeClass("open");
+  $(".p-hamburger__menu").slideUp();
+});
 $(".accordion").hover(function () {
   $(this).children(".accordion-title").toggleClass("open");
   $(this).children(".accordion-body").stop().slideToggle();
 });
 
 $(".l-header__list").hover(function () {
-  $(this).children(".p-dropdown__menu").stop().slideToggle(500);
+  $(this).children(".p-dropdown__menu").stop().slideToggle("fast");
 });
